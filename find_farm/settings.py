0f1edebb649d4c601b19703dd554b8077eaaf4f5
Barnabas Tserga <barnabasoluwatosin@gmail.com>
2023-02-09 10:45:32 +0100

    Migrate to postgresql

--- find_farm/settings.py
+++ find_farm/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'find_farm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT', cast=int),
     }
 }
 
